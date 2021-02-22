e17258f5a96843b37141aa172dff8a000fd4719d
jallend1 <jallend1@gmail.com>
2021-02-22 14:24:35 -0800

    Adds functionality to click buttons on FAQ page to display or hide relevant set of FAQs

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const burger = document.getElementById("burger");
+const $buttons = document.getElementsByClassName('faq-button') || null;
+
+const showFAQ = e => {
+  const $patronButton = document.getElementById('patrons');
+  const $librariesButton = document.getElementById('libraries')
+  if(e.target.id === 'patron-faq'){
+    $patronButton.classList.toggle('is-hidden');
+    $librariesButton.classList.add('is-hidden');
+  }
+  else if(e.target.id === 'library-faq'){
+    $librariesButton.classList.toggle('is-hidden');
+    $patronButton.classList.add('is-hidden');
+  }
+}
 
 const showMenu = () => {
   const $menu = document.getElementById("navbarContent");
@@ -7,3 +21,11 @@ const showMenu = () => {
 };
 
 burger.addEventListener("click", showMenu);
+
+if ($buttons){
+  for(button of $buttons){
+    button.addEventListener('click', showFAQ);
+  }
+}
+
+console.log($buttons);
