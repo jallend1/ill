3793f9904d18bf0b763322e9678b98e2930f7fc2
jallend1 <jallend1@gmail.com>
2021-02-23 21:28:14 -0800

    Cleans up basic code syntax for legibility

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const burger = document.getElementById("burger");
-const $faqButtons = document.getElementsByClassName('faq-button') || null;
+const $faqButtons = document.getElementsByClassName("faq-button") || null;
 
-const showFAQ = e => {
-  const $patronFAQs = document.getElementById('patrons');
-  const $patronButton = document.getElementById('patron-faq');
-  const $libraryFAQs = document.getElementById('libraries')
-  const $libraryButton = document.getElementById('library-faq');
-  if(e.target.id === 'patron-faq'){
-    $patronFAQs.classList.toggle('is-hidden');
-    $patronFAQs.classList.contains('is-hidden') ? $patronButton.innerText = "Show me patron FAQs" : $patronButton.innerText = "Hide patron FAQs";
-    $libraryFAQs.classList.add('is-hidden');
+const showFAQ = (e) => {
+  const $patronFAQs = document.getElementById("patrons");
+  const $patronButton = document.getElementById("patron-faq");
+  const $libraryFAQs = document.getElementById("libraries");
+  const $libraryButton = document.getElementById("library-faq");
+  if (e.target.id === "patron-faq") {
+    $patronFAQs.classList.toggle("is-hidden");
+    $patronFAQs.classList.contains("is-hidden")
+      ? ($patronButton.innerText = "Show me patron FAQs")
+      : ($patronButton.innerText = "Hide patron FAQs");
+    $libraryFAQs.classList.add("is-hidden");
     $libraryButton.innerText = "Show me library FAQs";
+  } else if (e.target.id === "library-faq") {
+    $libraryFAQs.classList.toggle("is-hidden");
+    $patronFAQs.classList.add("is-hidden");
+    $libraryFAQs.classList.contains("is-hidden")
+      ? ($libraryButton.innerText = "Show me library FAQs")
+      : ($libraryButton.innerText = "Hide library FAQs");
   }
-  else if(e.target.id === 'library-faq'){
-    $libraryFAQs.classList.toggle('is-hidden');
-    $patronFAQs.classList.add('is-hidden');
-    $libraryFAQs.classList.contains('is-hidden') ? $libraryButton.innerText = "Show me library FAQs" : $libraryButton.innerText = "Hide library FAQs";
-  }
-}
+};
 
 const showMenu = () => {
   const $menu = document.getElementById("navbarContent");
@@ -27,8 +30,8 @@ const showMenu = () => {
 
 burger.addEventListener("click", showMenu);
 
-if ($faqButtons){
-  for(button of $faqButtons){
-    button.addEventListener('click', showFAQ);
+if ($faqButtons) {
+  for (button of $faqButtons) {
+    button.addEventListener("click", showFAQ);
   }
 }
