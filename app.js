612e4b495b77841a52ccc27f728fadbd19c1d5f2
jallend1 <jallend1@gmail.com>
2021-02-27 22:21:49 -0800

    Cleans up code structure

--- app.js
+++ app.js
@@ -2,37 +2,41 @@ const burger = document.getElementById("burger");
 const $faqButtons = document.getElementsByClassName("faq-button") || null;
 
 const showFAQ = (e) => {
-  for(let i = 0; i < $faqButtons.length; i++){
+  for (let i = 0; i < $faqButtons.length; i++) {
     // If the button clicked matches the one in the HTML collection
-    if($faqButtons[i].id === e.target.id){
+    if ($faqButtons[i].id === e.target.id) {
       // Change the text to reflect its current state
-      $faqButtons[i].innerText.includes('Show') ? 
-        $faqButtons[i].innerText = "Hide" + $faqButtons[i].innerText.slice(4) 
-        : $faqButtons[i].innerText = "Show" + $faqButtons[i].innerText.slice(4); 
+      $faqButtons[i].innerText.includes("Show")
+        ? ($faqButtons[i].innerText =
+            "Hide" + $faqButtons[i].innerText.slice(4))
+        : ($faqButtons[i].innerText =
+            "Show" + $faqButtons[i].innerText.slice(4));
       // Show or Hide the div containing the corresponding information
       const FAQs = document.getElementById($faqButtons[i].dataset.target);
-      FAQs.classList.toggle('is-hidden');
+      FAQs.classList.toggle("is-hidden");
       // Extracts id of parent card div and toggles active styling
-      const $targetDiv = document.getElementById($faqButtons[i].id.split('-')[0]);
-      $targetDiv.classList.toggle('is-clicked');
+      const $targetDiv = document.getElementById(
+        $faqButtons[i].id.split("-")[0]
+      );
+      $targetDiv.classList.toggle("is-clicked");
     }
     // If it doesn't match
-    else{
+    else {
       // Reset its text to initial value
-      $faqButtons[i].innerText = "Show" + $faqButtons[i].innerText.slice(4) 
+      $faqButtons[i].innerText = "Show" + $faqButtons[i].innerText.slice(4);
       // Makes sure the corresponding FAQ is not displayed
       const FAQs = document.getElementById($faqButtons[i].dataset.target);
-      FAQs.classList.add('is-hidden');
+      FAQs.classList.add("is-hidden");
       // Extracts id of parent card div and removes any active styling
-      const $targetDiv = document.getElementById($faqButtons[i].id.split('-')[0]);
-      $targetDiv.classList.remove('is-clicked');
+      const $targetDiv = document.getElementById(
+        $faqButtons[i].id.split("-")[0]
+      );
+      $targetDiv.classList.remove("is-clicked");
     }
   }
 };
 
-const resetFAQs = () => {
-
-}
+const resetFAQs = () => {};
 
 const showMenu = () => {
   const $menu = document.getElementById("navbarContent");
